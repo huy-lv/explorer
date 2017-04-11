7e6efe629d006938f15112972c6234cac949ef79
huylv177 <huylv177@gmail.com>
2017-04-11 15:27:37 +0700

    Initial commit

---
+++ app/src/main/java/com/pctd/explorer/recycler/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package com.pctd.explorer.recycler;
+
+public interface OnItemClickListener {
+
+    void onItemClick(int position);
+
+    boolean onItemLongClick(int position);
+}
---
+++ app/src/main/java/com/pctd/explorer/recycler/OnItemSelectedListener.java
@@ -0,0 +1,6 @@
+package com.pctd.explorer.recycler;
+
+public interface OnItemSelectedListener {
+
+    void onItemSelected();
+}
